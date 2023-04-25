21f5355cbf2a9eeb583fee4bcf186c2f763ea26b
Reda96R <redarayyad@yahoo.com>
2023-04-25 18:50:51 +0100

    working parsing

---
+++ fdf.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rerayyad <rerayyad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/12 23:28:02 by rerayyad          #+#    #+#             */
+/*   Updated: 2023/04/25 18:25:48 by rerayyad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+#include <mlx.h>
+
+int	main(int ac, char *av[])
+{
+	t_data	**fdf;
+	int		fd;
+
+	fd = open (av[1], O_RDONLY, 0);
+	if (ac != 2 || !ft_extension_check(av[1]) || fd <= 0)
+	{
+		ft_false_input(ac, fd);
+		close (fd);
+		exit (1);
+	}
+	else if (!ft_lenght_check(av[1], ft_width(av[1])))
+		ft_errors_buster(1);
+	else
+			fdf = ft_maps_reader(av[1], fd);
+	ft_cleaner(&fdf, ft_height(av[1]));
+	return (0);
+}
---
+++ ft_reader_tools.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_reader_tools.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rerayyad <rerayyad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/16 23:30:57 by rerayyad          #+#    #+#             */
+/*   Updated: 2023/04/25 16:52:29 by rerayyad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+int	ft_width(char *file)
+{
+	char	*line;
+	int		fd;
+	int		width;
+
+	fd = open(file, O_RDONLY, 0);
+	get_next_line(fd, &line);
+	width = ft_word_counter(line, ' ');
+	free(line);
+  close (fd);
+	return (width);
+}
+
+int	ft_height(char *file)
+{
+	char	*line;
+	int		fd;
+	int		height;
+
+	height = 0;
+	fd = open(file, O_RDONLY, 0);
+	while (get_next_line(fd, &line))
+	{
+		height++;
+		free(line);
+	}
+	close (fd);
+	return (height);
+}
