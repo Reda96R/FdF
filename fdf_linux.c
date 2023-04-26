a64d857e688b6663dfcf49b0f2c9e509629c1855
Reda96R <redarayyad@yahoo.com>
2023-04-26 18:31:24 +0000

    sync

--- fdf_linux.c
+++ fdf_linux.c
@@ -6,16 +6,13 @@
 /*   By: rerayyad <rerayyad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 23:28:02 by rerayyad          #+#    #+#             */
-<<<<<<< HEAD
 /*   Updated: 2023/04/25 18:25:48 by rerayyad         ###   ########.fr       */
-=======
 /*   Updated: 2023/04/25 16:52:15 by rerayyad         ###   ########.fr       */
->>>>>>> cac0e5efa918c59dd155b3b7d987123cac80bb5c
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
-#include <mlx.h>
+#include "mlx_linux/mlx.h"
 
 int	main(int ac, char *av[])
 {
@@ -30,11 +27,7 @@ int	main(int ac, char *av[])
 		exit (1);
 	}
 	else if (!ft_lenght_check(av[1], ft_width(av[1])))
-<<<<<<< HEAD
-		ft_errors_buster(1);
-=======
 		ft_errors_buster(3);
->>>>>>> cac0e5efa918c59dd155b3b7d987123cac80bb5c
 	else
 			fdf = ft_maps_reader(av[1], fd);
 	ft_cleaner(&fdf, ft_height(av[1]));
